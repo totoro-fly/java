991d3cf7fa53064def7a0fc4ff2c14437fd121da
totoro-fly <liujian180389@gmail.com>
2016-07-18 23:13:58 +0800

    7.18

---
+++ src/chapter/two/Test2_2.java
@@ -0,0 +1,14 @@
+package chapter.two;
+
+import java.util.Scanner;
+
+public class Test2_2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("请键入的值将被转化为双精度");
+		double d=input.nextDouble();
+		System.out.println(d);
+	}
+
+}

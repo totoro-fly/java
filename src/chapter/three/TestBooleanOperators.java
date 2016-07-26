8cc310fce341dec5d51bc7aba09570977cb51882
totoro-fly <liujian180389@gmail.com>
2016-07-26 13:28:49 +0800

    7261328

---
+++ src/chapter/three/TestBooleanOperators.java
@@ -0,0 +1,23 @@
+package chapter.three;
+
+import java.util.Scanner;
+
+public class TestBooleanOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer");
+		int number = input.nextInt();
+		if (number % 2 == 0 || number % 3 == 0)
+			System.out.println(number + " is divisible by 2 or 3.");
+		if (number % 2 == 0 && number % 3 == 0)
+			System.out.println(number + " is divisible by 2 and 3.");
+
+		if (number % 2 == 0 ^ number % 3 == 0)
+			System.out.println(number + " is divisible by 2 or 3,but not both.");
+		
+
+	}
+
+}

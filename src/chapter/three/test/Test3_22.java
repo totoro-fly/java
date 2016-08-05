5adc8ef767be386a8794b02ef8c5027cb80d8d70
totoro-fly <liujian180389@gmail.com>
2016-08-05 23:48:20 +0800

    book,today just only.

--- src/chapter/three/test/Test3_22.java
+++ src/chapter/three/test/Test3_22.java
@@ -1,10 +1,20 @@
 package chapter.three.test;
 
+import java.util.Scanner;
+
 public class Test3_22 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a point with two coordinates:");
+		double x = input.nextDouble();
+		double y = input.nextDouble();
+		double l = Math.pow((Math.pow(x, 2) + Math.pow(y, 2)), 0.5);
+		if(l<=10)
+			System.out.println("Point ("+x+" , "+y+") is in the circle.");
+		else
+			System.out.println("Point ("+x+" , "+y+") is not in the circle.");
 	}
 
 }

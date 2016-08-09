3db6a9449a37a6ff3535891d8dc80534bc7f79d8
totoro-fly <liujian180389@gmail.com>
2016-08-09 23:50:27 +0800

    PC

---
+++ src/chapter/four/Test4_1.java
@@ -0,0 +1,29 @@
+package chapter.four;
+
+public class Test4_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Math.sqrt(4));
+		System.out.println(Math.sin(Math.PI));
+		System.out.println(Math.sin(2 * Math.PI));
+		System.out.println(Math.cos(2 * Math.PI));
+		System.out.println(Math.pow(2, 2));
+		System.out.println(Math.log(Math.E));
+		System.out.println(Math.exp(1));
+		System.out.println(Math.max(2, Math.min(3, 4)));
+		System.out.println(Math.rint(-2.5));
+		System.out.println(Math.ceil(-2.5));// -2
+		System.out.println(Math.floor(-2.5));// -3
+		System.out.println(Math.round(-2.5f));
+		System.out.println(Math.round(-2.5));
+		System.out.println(Math.rint(2.5));
+		System.out.println(Math.ceil(2.5));
+		System.out.println(Math.floor(2.5));
+		System.out.println(Math.round(2.5f));
+		System.out.println(Math.round(2.5));
+		System.out.println(Math.round(Math.abs(-2.5)));
+
+	}
+
+}

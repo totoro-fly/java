4bb319f45e13ff8e19989217e94dbb66afcfe966
totoro-fly <liujian180389@gmail.com>
2016-08-18 22:55:14 +0800

    PC over other

---
+++ src/chapter/four/test/Test4_1.java
@@ -0,0 +1,16 @@
+package chapter.four.test;
+
+import java.util.Scanner;
+
+public class Test4_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the length from the center to a vertex:");
+		double s=2*input.nextDouble()*Math.sin(Math.PI/5);
+		double area=(5*s*s)/(4*Math.tan(Math.PI/5));
+		System.out.printf("The area of the pentagon is %5.2f.",area);
+	}
+
+}
